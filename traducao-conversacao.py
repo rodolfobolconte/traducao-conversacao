5b69cfcde80990a8789993f2281a90bc58cce3e5
Rodolfo Bolconte <rodolfobolconte@gmail.com>
2021-02-17 11:45:42 -0300

    criando arquivo para converter voz em texto

--- traducao-conversacao.py
+++ traducao-conversacao.py
@@ -1,20 +1,18 @@
+from googletrans import Translator
 from gtts import gTTS
 from playsound import playsound
 import os
 
-tts = gTTS('Bom dia Rodolfo, você está bem?', lang='pt-br')
+texto = input('Digite um texto: ')
 
+tts = gTTS(texto, lang='pt-br')
 arquivo = 'audio.mp3'
 
-tts.save(arquivo)
-playsound(arquivo)
-os.remove(arquivo)
-exit()
+#tts.save(arquivo)
+#playsound(arquivo)
+#os.remove(arquivo)
 
-from googletrans import Translator
-
-texto = "How old are you?"
 
 tradutor = Translator()
 
-print(tradutor.translate(texto, dest='pt').text)
+print(tradutor.translate(texto, dest='en').text)
